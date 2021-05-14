1a1472a101f794ddb0b4070b240b15bf38e2274d
F3licia <romeofelicia@gmail.com>
2021-05-14 19:36:05 +0200

    stampa a schermo immagini e banndiere

--- scriptBoolflix.js
+++ scriptBoolflix.js
@@ -4,35 +4,17 @@ const app = new Vue({
           tmdbApiK:"27c55bf69cc942b29c61c82472c4114c",
           textToSearch:"",
           movieList:[],
+          langList:[],
           tvSeriesList:[],
           },
 
           methods:{
 
-            getFlags(maindata) {    //si può migliorare
-                maindata.forEach((el) => {
-                    switch(el.original_language) {
-                        case "en":
-                        el.original_language = "https://www.countryflags.io/US/flat/64.png"
-                        break;
-                        case "fr":
-                        el.original_language = "https://www.countryflags.io/FR/flat/64.png"
-                        break;
-                        case "de":
-                        el.original_language = "https://www.countryflags.io/DE/flat/64.png"
-                        break;
-                        case "it":
-                        el.original_language = "https://www.countryflags.io/IT/flat/64.png"
-                        break;
-                        case "es":
-                        el.original_language = "https://www.countryflags.io/ES/flat/64.png"
-                        break;
-                        case "pt":
-                        el.original_language = "https://www.countryflags.io/PT/flat/64.png"
-                        break;
-                        default:
-                        el.original_language
-                      }
+            getLanguages() {
+                this.movieList.forEach((movie) => {
+                    if(!this.langList.includes(movie.original_language)){
+                     this.langList.push(movie.original_language);       
+                    }
                 })
             },
 
@@ -42,28 +24,40 @@ const app = new Vue({
                         api_key: this.tmdbApiK,
                         query: this.textToSearch,
                         language: "it-IT"
-                    }
+                    }                                               //----------DA RIVEDERE
                 }
-                 axios.get("https://api.themoviedb.org/3/search/" + type, axiosOptions).then((resp)=> {
+                axios.get("https://api.themoviedb.org/3/search/" + type, axiosOptions).then((resp)=> {   
                    
                          if(type==="movie"){
-                           this.movieList = resp.data.results  
+                              this.movieList = resp.data.results.map((movie) => {
+                                  if(movie.original_language ==="en"){movie.original_language ="us"}
+                                  if(movie.original_language ==="ja"){movie.original_language ="jp"}
+                                 movie.original_language = "https://www.countryflags.io/"+movie.original_language+"/flat/64.png";
+                                movie.poster_path = "https://image.tmdb.org/t/p/w342"+movie.poster_path;
+                                  
+                                  return movie
+                                  }
+                              )
                          }else if(type==="tv"){
                              this.tvSeriesList = resp.data.results.map((serie) => {
+                                if(movie.original_language ==="en"){movie.original_language ="us"}
+                                if(movie.original_language ==="ja"){movie.original_language ="jp"}
+                                serie.original_language = "https://www.countryflags.io/"+serie.original_language+"/flat/64.png";
+                                serie.poster_path = "https://image.tmdb.org/t/p/w342"+serie.poster_path;
                                  serie.title = serie.name  //modifica dei dati PRIMA di inserirli in data
                                  serie.original_title = serie.original_name 
                                  return serie
                                  }
                              )};
-                             this.getFlags(this.movieList)
-                    })
-             },
+                             this.getLanguages()
+                })
+            },
            
             doSearch() {
             
             this.makeAxiosSearch("movie")
             this.makeAxiosSearch("tv")
-            this.getFlags(this.movieList)
+            
             
           
             },
@@ -73,21 +67,3 @@ const app = new Vue({
  
 
 
-
-   
-
-
-
-
-
-
-//  <img src="https://www.countryflags.io/US/flat/64.png">
-
-/*
-if(type==="tv"){
-      this.movieList = resp.data.results.map((el) => {
-          el.original_language === "en"  //modifica dei dati PRIMA di inserirli in data
-          el.original_language = "https://www.countryflags.io/US/flat/64.png"
-          return serie
-          }
-      )};*/
