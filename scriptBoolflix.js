371407b6de09b4d29bc7a25762c71976eaa65c13
F3licia <romeofelicia@gmail.com>
2021-05-14 11:13:26 +0200

    milestone2

--- scriptBoolflix.js
+++ scriptBoolflix.js
@@ -5,28 +5,46 @@ const app = new Vue({
           textToSearch:"",
           movieList:[],
           tvSeriesList:[],
-          langList:[],
           },
 
           methods:{
-    
-            getLanguages() {
-                this.movieList.forEach((movie) => {
-                    if(!this.langList.includes(movie.original_language)){
-                     this.langList.push(movie.original_language);       
-                    }//va chiamata NEL then, alla fine
+
+            getFlags(maindata) {    //si può migliorare
+                maindata.forEach((el) => {
+                    switch(el.original_language) {
+                        case "en":
+                        el.original_language = "https://www.countryflags.io/US/flat/64.png"
+                        break;
+                        case "fr":
+                        el.original_language = "https://www.countryflags.io/FR/flat/64.png"
+                        break;
+                        case "de":
+                        el.original_language = "https://www.countryflags.io/DE/flat/64.png"
+                        break;
+                        case "it":
+                        el.original_language = "https://www.countryflags.io/IT/flat/64.png"
+                        break;
+                        case "es":
+                        el.original_language = "https://www.countryflags.io/ES/flat/64.png"
+                        break;
+                        case "pt":
+                        el.original_language = "https://www.countryflags.io/PT/flat/64.png"
+                        break;
+                        default:
+                        el.original_language
+                      }
                 })
             },
 
              makeAxiosSearch(type){
-                const axiosOption={
+                const axiosOptions={
                     params: {
                         api_key: this.tmdbApiK,
                         query: this.textToSearch,
                         language: "it-IT"
                     }
                 }
-               axios.get("https://api.themoviedb.org/3/search/" + type, axiosOption).then((resp)=> {
+                 axios.get("https://api.themoviedb.org/3/search/" + type, axiosOptions).then((resp)=> {
                    
                          if(type==="movie"){
                            this.movieList = resp.data.results  
@@ -37,16 +55,18 @@ const app = new Vue({
                                  return serie
                                  }
                              )};
-
-                          this.getLanguages()
-                    }) 
+                             this.getFlags(this.movieList)
+                    })
              },
            
             doSearch() {
+            
             this.makeAxiosSearch("movie")
             this.makeAxiosSearch("tv")
+            this.getFlags(this.movieList)
+            
           
-              },
+            },
             
         } 
 })         
@@ -54,4 +74,20 @@ const app = new Vue({
 
 
 
+   
 
+
+
+
+
+
+//  <img src="https://www.countryflags.io/US/flat/64.png">
+
+/*
+if(type==="tv"){
+      this.movieList = resp.data.results.map((el) => {
+          el.original_language === "en"  //modifica dei dati PRIMA di inserirli in data
+          el.original_language = "https://www.countryflags.io/US/flat/64.png"
+          return serie
+          }
+      )};*/
