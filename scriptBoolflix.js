7e7c69ff6b93bec2a454319af0078ae569c6f7df
F3licia <romeofelicia@gmail.com>
2021-05-14 02:20:40 +0200

    aggiunta axiosOption

--- scriptBoolflix.js
+++ scriptBoolflix.js
@@ -4,6 +4,7 @@ const app = new Vue({
           tmdbApiK:"27c55bf69cc942b29c61c82472c4114c",
           textToSearch:"",
           movieList:[],
+          tvSeriesList:[],
           langList:[],
           },
 
@@ -16,18 +17,34 @@ const app = new Vue({
                     }//va chiamata NEL then, alla fine
                 })
             },
-            doSearch() {
-                const axiosOptions={
+
+             makeAxiosSearch(type){
+                const axiosOption={
                     params: {
                         api_key: this.tmdbApiK,
                         query: this.textToSearch,
                         language: "it-IT"
                     }
                 }
-               axios.get("https://api.themoviedb.org/3/search/movie?", axiosOptions).then((resp)=> {
-                          this.movieList = resp.data.results,
+               axios.get("https://api.themoviedb.org/3/search/" + type, axiosOption).then((resp)=> {
+                   
+                         if(type==="movie"){
+                           this.movieList = resp.data.results  
+                         }else if(type==="tv"){
+                             this.tvSeriesList = resp.data.results.map((serie) => {
+                                 serie.title = serie.name  //modifica dei dati PRIMA di inserirli in data
+                                 serie.original_title = serie.original_name 
+                                 return serie
+                                 }
+                             )};
+
                           this.getLanguages()
                     }) 
+             },
+           
+            doSearch() {
+            this.makeAxiosSearch("movie")
+            this.makeAxiosSearch("tv")
           
               },
             
